b09585470c620188f183f29756b94320493cd9b2
expert700 <dyllancole@outlook.com>
2016-05-23 18:27:35 -0400

    A test.

--- app/src/main/java/com/nope/asynchronousengineclient/main.java
+++ app/src/main/java/com/nope/asynchronousengineclient/main.java
@@ -2,14 +2,18 @@ package com.nope.asynchronousengineclient;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class main extends AppCompatActivity
 {
-
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void buttonPress (View v) {
+        System.out.println("The button was pressed.");
     }
 }
