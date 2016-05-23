7cbe393058a34617a7c6b396e0b604631318eaaa
expert700 <dyllancole@outlook.com>
2016-05-23 18:05:30 -0400

    Initial commit

---
+++ app/src/main/java/com/nope/asynchronousengineclient/main.java
@@ -0,0 +1,15 @@
+package com.nope.asynchronousengineclient;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class main extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
