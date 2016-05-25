83b8fbabcc478f5cba65a0a8502f194f759488ad
expert700 <dyllancole@outlook.com>
2016-05-25 18:32:46 -0400

    Yay for boredom.

--- app/src/main/java/com/nope/asynchronousengineclient/main.java
+++ app/src/main/java/com/nope/asynchronousengineclient/main.java
@@ -1,16 +1,13 @@
 package com.nope.asynchronousengineclient;
 
-import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 
 import java.io.BufferedReader;
-import java.io.DataOutputStream;
 import java.io.InputStreamReader;
-import java.net.InetSocketAddress;
-import java.net.Socket;
+import java.net.URL;
+import java.util.ArrayList;
 
 public class main extends AppCompatActivity
 {
@@ -19,11 +16,30 @@ public class main extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-        StrictMode.setThreadPolicy(policy);
     }
 
     public void buttonPress (View v) {
-        new TCPClient().execute("handshake", "172.21.19.26", "6789");
+        ArrayList<String> info = getServerAddress();
+        new TCPClient().execute("handshake", info.get(0), info.get(1));
+    }
+
+    private ArrayList<String> getServerAddress() {
+        ArrayList<String> details = new ArrayList<>();
+        try {
+            URL infourl = new URL("http://www.expert700.me/serverinfo.php/");
+            BufferedReader in = new BufferedReader( new InputStreamReader(infourl.openStream()));
+            String input = in.readLine();
+
+            int split = input.indexOf(":");
+            String ip = input.substring(0, split);
+            String port = input.substring(split + 1, input.length());
+            details.add(0, ip);
+            details.add(1, port);
+        } catch (Exception e) {
+            e.printStackTrace();
+            details.add(0, "173.76.28.219");
+            details.add(1, "6789");
+        }
+        return details;
     }
 }
